205d9d779ec3bcb747c64d5c55e8ae15b1f359ec
helwan-djibril-mimouni <163547268+helwan-djibril-mimouni@users.noreply.github.com>
2024-06-03 16:29:07 +0200

    refactor(main.cpp): added background and spots for future turrets

--- main.cpp
+++ main.cpp
@@ -19,15 +19,20 @@ int main()
     path.add(1, 200);
     path.add(2, 200);
     path.add(3, 500);
-    path.add(0, 400);
+    path.add(0, 300);
 
     Spawner spawner = Spawner(path, 0, 50);
+
+    Image image = LoadImage("assets/background.png");
+    Texture2D texture = LoadTextureFromImage(image);
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         BeginDrawing();
         ClearBackground(BLACK);
+
+        DrawTexture(texture, 0, 0, WHITE);
 
         spawner.update();
 
@@ -38,5 +43,7 @@ int main()
 
         EndDrawing();
     }
+
+    return 0;
 }
 // need class for path, class for animal following path
